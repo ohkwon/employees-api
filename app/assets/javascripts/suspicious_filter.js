3cb8750f967354cc0b0d0d4675657bff3fba2d5d
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-09-08 20:44:01 -0700

    attempt to add parameters to custom filter. did not succeed

--- app/assets/javascripts/suspicious_filter.js
+++ app/assets/javascripts/suspicious_filter.js
@@ -1,7 +1,23 @@
-angular.module("app").filter('suspicious', [function () {
+angular.module("app").filter('suspicious', [function (age) {
   return function(employees) {
+    console.log(age);
+    var filteredEmployees = [];
+    var today = new Date();
+    var date = today.getDate();
+    var month = today.getMonth() + 1;
+    var year = today.getFullYear() - age;
+    var cutOffDate = new Date(year, month, date);
+    console.log(cutOffDate);
+    console.log(employees);
     if (employees) {
-      return [employees[6]];
+      for (var i = 0; i < employees.length; i++) {
+        var birthday = new Date(employees[i].birthdate)
+        console.log(birthday);
+        if (birthday >= cutOffDate) {
+          filteredEmployees.push(employees[i]);
+        }
+      }
     }
+    return filteredEmployees;
   }
 }]);
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+100.times do 
+  Employee.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.backward(55000),
+    ssn: Faker::Code.ean
+  )
+end 
 
 200.times do
   Address.create(
